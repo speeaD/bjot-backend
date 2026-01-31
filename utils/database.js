0488649b7569be1d8cadf09e8cb280637f266f90
speeaD <sejiofor20@gmail.com>
2026-01-31 06:24:26 +0100

    update

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 // utils/database.js
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../generated/client');
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
