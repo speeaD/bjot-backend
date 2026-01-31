764fc1a4a28611932731abbc155f2d9a7b406c58
speeaD <sejiofor20@gmail.com>
2026-01-31 06:29:25 +0100

    try fix: prisma fix

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 // utils/database.js
-const { PrismaClient } = require('../generated/client');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
